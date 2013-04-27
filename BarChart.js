b61761f3a46a072879c7851b3d8f97cd416ba559
Denisson <denisson.alexandre@gmail.com>
2013-04-27 18:20:35 -0300

    Adding minified version

--- BarChart.js
+++ BarChart.js
@@ -22,7 +22,8 @@ google._setOnLoadCallback = google.setOnLoadCallback;
 									//calcular a soma dos valores de cada coluna ignorando a primeira que é o label do eixo Y
 									_total = getTotal(row);
 									//Criar elemento <text> com a porcentagem da coluna
-										for (var ci = 1, cj = ri, cLen = row.length - 1; ci <= cLen; ci++, cj+=__bars__.length / cLen) {
+										for (var ci = 1, cj = ri, cLen = row.length - 1; 
+												 ci <= cLen; ci++, cj+=__bars__.length / cLen) {
 											   var _el = __bars__[cj];
 											   var _value = formatFloat((row[ci].v / _total) * 100);
 											   if(_value <= 0)
@@ -30,12 +31,12 @@ google._setOnLoadCallback = google.setOnLoadCallback;
 
 										var pos = {
 											x:  (parseFloat($(_el).attr("x")) + parseFloat($(_el).attr("width"))) + 5,
-											y:  parseFloat($(_el).attr("y")) + (parseFloat($(_el).attr("height"))/1.1)										
+											y:  parseFloat($(_el).attr("y")) + (parseFloat($(_el).attr("height"))/1.1)
 										};
 										var __parent__ = $(_el).parent();
 										__parent__.append(addTextNode({x:pos.x, y:pos.y, stroke:'none','font-family':"Segoe UI light", fill:'black', 'font-size':12 }, _value + "%", _el))
-									};
-						};
+									}
+						}
 	  				};
 
 		function formatFloat(n) {
