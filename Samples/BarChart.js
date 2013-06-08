a14549d22133b9bfd4d00e2fadb81cacf4d0c29f
Denisson <denisson.alexandre@gmail.com>
2013-06-08 16:57:55 -0300

    testing with ie8

--- Samples/BarChart.js
+++ Samples/BarChart.js
@@ -2,27 +2,34 @@ google._setOnLoadCallback = google.setOnLoadCallback;
   google.setOnLoadCallback = function(callback){
   		function __overrides_chart_draw(){
 		//BarChart Doesn't have a getDataTable Method, so i'm creating one.
-		google.visualization.BarChart.prototype.getDataTable = function(){return this.pa.Sc};
+		google.visualization.BarChart.prototype.getDataTable = function(){return this.ga.cd};
 		google.visualization.BarChart.prototype.__chart_options = {};
   		google.visualization.BarChart.prototype.getOptions = function(){return this.__chart_options};
 	      google.visualization.BarChart.prototype._draw = 
 	      	google.visualization.BarChart.prototype.draw; 
 	      		google.visualization.BarChart.prototype.draw = 
 	  				function (a, c) {
-	  					var __chart__ = this;
+						var __chart__ = this, _div;
+	  					for (var property in __chart__) {
+						    if (__chart__.hasOwnProperty(property)) {
+						        if(__chart__[property] && __chart__[property].nodeType){
+						        	_div = __chart__[property]
+						        }
+						    }
+						}
 	  					__chart__.__chart_options = $.extend({}, c);
 	  					__chart__._draw(a, c);
-	  					
-						var __table__ = __chart__.getDataTable().D;
+						var __table__ = a.toPOJO().rows;
 						
-						var __bars__ = $("svg > g:nth(2) > g > g:nth(1) > rect",$("#"+ __chart__.cd.id));
+						var __bars__ = $("svg > g:nth(2) > g > g:nth(1) > rect",$("#"+ _div.id));
 						for (var ri = 0,rLen = __table__.length -1 ; ri <= rLen; ri++) {
 								var row = __table__[ri].c;
 								var _total = 0;
 									//calcular a soma dos valores de cada coluna ignorando a primeira que é o label do eixo Y
 									_total = getTotal(row);
 									//Criar elemento <text> com a porcentagem da coluna
-										for (var ci = 1, cj = ri, cLen = row.length - 1; ci <= cLen; ci++, cj+=__bars__.length / cLen) {
+										for (var ci = 1, cj = ri, cLen = row.length - 1; 
+												 ci <= cLen; ci++, cj+=__bars__.length / cLen) {
 											   var _el = __bars__[cj];
 											   var _value = formatFloat((row[ci].v / _total) * 100);
 											   if(_value <= 0)
@@ -30,12 +37,12 @@ google._setOnLoadCallback = google.setOnLoadCallback;
 
 										var pos = {
 											x:  (parseFloat($(_el).attr("x")) + parseFloat($(_el).attr("width"))) + 5,
-											y:  parseFloat($(_el).attr("y")) + (parseFloat($(_el).attr("height"))/1.1)										
+											y:  parseFloat($(_el).attr("y")) + (parseFloat($(_el).attr("height"))/1.1)
 										};
 										var __parent__ = $(_el).parent();
 										__parent__.append(addTextNode({x:pos.x, y:pos.y, stroke:'none','font-family':"Segoe UI light", fill:'black', 'font-size':12 }, _value + "%", _el))
-									};
-						};
+									}
+						}
 	  				};
 
 		function formatFloat(n) {
--- Samples/ChartWrapper.js
+++ Samples/ChartWrapper.js
@@ -5,12 +5,18 @@ google._setOnLoadCallback = google.setOnLoadCallback;
 	      	google.visualization.ChartWrapper.prototype.draw; 
 	      		google.visualization.ChartWrapper.prototype.draw = 
 	  				function() {
-	  					var __chart__ = this;
+	  					var __chart__ = this, _div;
+	  					for (var property in __chart__) {
+						    if (__chart__.hasOwnProperty(property)) {
+						        if(__chart__[property] && __chart__[property].nodeType){
+						        	_div = __chart__[property]
+						        }
+						    }
+						}
 	  					__chart__._draw(arguments);
 	  					
-						var __table__ = __chart__.getDataTable().D;
-						
-						var __bars__ = $("svg > g:nth(2) > g > g:nth(1) > rect",$("#"+ __chart__.dg.id));
+						var __table__ = __chart__.getDataTable().toPOJO().rows;
+						var __bars__ = $("svg > g:nth(2) > g > g:nth(1) > rect",$("#"+ _div.id));
 						for (var ri = 0,rLen = __table__.length -1 ; ri <= rLen; ri++) {
 								var row = __table__[ri].c;
 								var _total = 0;
